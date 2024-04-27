5d0375091e4408f1b11254d95782886993b76438
Arthur <arthur.brandao@mobicare.com.br>
2024-04-27 10:38:22 -0300

    adicionando mais validações, como valores altos e numéricos

--- src/test/java/tests/test.java
+++ src/test/java/tests/test.java
@@ -2,6 +2,8 @@ package tests;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.Console;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.CoreMatchers.*;
 
@@ -29,6 +31,98 @@ class PostTest {
             body("job", equalTo("leader")).
             body("id", notNullValue()).
             body("createdAt", notNullValue());
+    }
+
+    @Test
+    void emptyBodySuccessTest() {
+        String payload = "{}";
+
+        given().
+                contentType("application/json").
+                body(payload).
+                when().
+                post().
+                then().
+                statusCode(201).
+                body("id", notNullValue()).
+                body("createdAt", notNullValue());
+    }
+
+    @Test
+    void bigNameSuccessTest() {
+        String payload = "{ \"name\": \"morpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheus" +
+                "morpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheus" +
+                "morpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheus" +
+                "morpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheusmorpheus" +
+                "\", \"job\": \"leader\" }";
+
+        given().
+                contentType("application/json").
+                body(payload).
+                when().
+                post().
+                then().
+                statusCode(201).
+                body("id", notNullValue()).
+                body("createdAt", notNullValue());
+    }
+
+    @Test
+    void bigJobSuccessTest() {
+        String payload = "{ \"name\": \"morpheus\", \"job\": \"leaderleaderleaderleaderleaderleaderleaderleaderleader" +
+                "leaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleader" +
+                "leaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleader" +
+                "leaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleaderleader\" }";
+
+        given().
+                contentType("application/json").
+                body(payload).
+                when().
+                post().
+                then().
+                statusCode(201).
+                body("id", notNullValue()).
+                body("createdAt", notNullValue());
+    }
+
+    @Test
+    void nameAsNumberSuccessTest() {
+        int name = 1;
+        String  job = "leader";
+
+        String payload = "{ \"name\": " + name + ", \"job\": \"" + job + "\" }";
+        System.out.println(payload);
+        given().
+                contentType("application/json").
+                body(payload).
+                when().
+                post().
+                then().
+                statusCode(201).
+                body("name", equalTo(name)).
+                body("job", equalTo(job)).
+                body("id", notNullValue()).
+                body("createdAt", notNullValue());
+    }
+
+    @Test
+    void jobAsNumberSuccessTest() {
+        String name = "Antonio";
+        int  job = 2;
+
+        String payload = "{ \"name\": \"" + name + "\", \"job\": " + job + " }";
+        System.out.println(payload);
+        given().
+                contentType("application/json").
+                body(payload).
+                when().
+                post().
+                then().
+                statusCode(201).
+                body("name", equalTo(name)).
+                body("job", equalTo(job)).
+                body("id", notNullValue()).
+                body("createdAt", notNullValue());
     }
 
     @Test
